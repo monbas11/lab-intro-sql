e5f13caadb1d76763cc7561699c4d40d6ceb9f30
Monica Basile <basilemonica11@gmail.com>
2023-12-02 15:46:00 +0100

    lab done

---
+++ lab1.sql
@@ -0,0 +1,9 @@
+use sakila;
+select * from actor;
+select * from film;
+select *  from customer;
+select title from film;
+select name from language as language;
+select count(store_id) from store;
+select count(staff_id) from staff;
+select first_name from staff;
